6aae5436f904b69b1269fda14cf9cda7ef9882d8
Анастасия Ельшина <salnaya92@gmail.com>
2023-06-06 16:50:37 +0300

    eslint errors fixed

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,14 @@
 
 import GamePlay from "./gameplay/GamePlay";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const board = document.querySelector('.board');
+document.addEventListener("DOMContentLoaded", () => {
+  const board = document.querySelector(".board");
   let deadCount = 0;
   let lostCount = 0;
-  let dead = document.querySelector('.dead-number');
-  let lost = document.querySelector('.lost-number');
+  let dead = document.querySelector(".dead-number");
+  let lost = document.querySelector(".lost-number");
   const gameplay = new GamePlay(board);
   gameplay.drawField();
-
-
 
   const getRandom = () => Math.floor(Math.random() * gameplay.cells.length);
   const interval = setInterval(() => {
@@ -19,38 +17,35 @@ document.addEventListener('DOMContentLoaded', () => {
     gameplay.cells.forEach((el, i, arr) => {
       if (i === oneRandom) {
         if (el === arr[i]) {
-          el.classList.add('cell-with-goblin');
+          el.classList.add("cell-with-goblin");
         }
       } else {
-        el.classList.remove('cell-with-goblin')
+        el.classList.remove("cell-with-goblin");
       }
-    })
+    });
   }, 800);
 
-
-
-
-  gameplay.cells.forEach(el => el.addEventListener('click', (e) => {
-    if (el.classList.contains('cell-with-goblin')) {
-      deadCount += 1;
-      dead.textContent = deadCount;
-      if (deadCount >= 10) {
-        clearInterval(interval);
-        alert('Вы забодали гоблина!');
-        dead.textContent = 0;
-        lost.textContent = 0;
+  gameplay.cells.forEach((el) =>
+    el.addEventListener("click", () => {
+      if (el.classList.contains("cell-with-goblin")) {
+        deadCount += 1;
+        dead.textContent = deadCount;
+        if (deadCount >= 10) {
+          clearInterval(interval);
+          alert("Вы забодали гоблина!");
+          dead.textContent = 0;
+          lost.textContent = 0;
+        }
+      } else {
+        lostCount += 1;
+        lost.textContent = lostCount;
+        if (lostCount >= 10) {
+          clearInterval(interval);
+          alert("Гоблин не забодан!");
+          dead.textContent = 0;
+          lost.textContent = 0;
+        }
       }
-    } else {
-      lostCount += 1;
-      lost.textContent = lostCount;
-      if (lostCount >= 10) {
-        clearInterval(interval);
-        alert('Гоблин не забодан!');
-        dead.textContent = 0;
-        lost.textContent = 0;
-      }
-    }
-  }))
-})
-
-
+    })
+  );
+});
--- src/js/gameplay/GamePlay.js
+++ src/js/gameplay/GamePlay.js
@@ -1,16 +1,17 @@
 export default class GamePlay {
-    constructor(element) {
-        this.element = element;
-        this.boardSize = 4;
-        this.cells = [];
-    }
+  constructor(element) {
+    this.element = element;
+    this.boardSize = 4;
+    this.cells = [];
+  }
 
-    drawField() {
-        for (let i = 0; i < this.boardSize ** 2; i += 1) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            this.element.appendChild(cell);
-        }
-        this.cells = Array.from(this.element.children)
+  drawField() {
+    const boardsize = Math.pow(this.boardSize, 2);
+    for (let i = 0; i < boardsize; i += 1) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      this.element.appendChild(cell);
     }
+    this.cells = Array.from(this.element.children);
+  }
 }
