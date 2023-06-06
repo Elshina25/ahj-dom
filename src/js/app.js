c042738d116f58348de4aeb50668b7ea6f8e39f8
Анастасия Ельшина <salnaya92@gmail.com>
2023-06-06 16:46:39 +0300

    update css

--- src/js/app.js
+++ src/js/app.js
@@ -2,51 +2,55 @@
 
 import GamePlay from "./gameplay/GamePlay";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const board = document.querySelector(".board");
+document.addEventListener('DOMContentLoaded', () => {
+  const board = document.querySelector('.board');
   let deadCount = 0;
   let lostCount = 0;
-  let dead = document.querySelector(".dead-number");
-  let lost = document.querySelector(".lost-number");
+  let dead = document.querySelector('.dead-number');
+  let lost = document.querySelector('.lost-number');
   const gameplay = new GamePlay(board);
   gameplay.drawField();
 
-  const getRandom = () => Math.floor(Math.random() * gameplay.cells.length);
 
+
+  const getRandom = () => Math.floor(Math.random() * gameplay.cells.length);
   const interval = setInterval(() => {
     let oneRandom = getRandom();
     gameplay.cells.forEach((el, i, arr) => {
       if (i === oneRandom) {
         if (el === arr[i]) {
-          el.classList.add("cell-with-goblin");
+          el.classList.add('cell-with-goblin');
         }
       } else {
-        el.classList.remove("cell-with-goblin");
-      }
-    });
-  }, 1000);
-
-  gameplay.cells.forEach((el) =>
-    el.addEventListener("click", (e) => {
-      if (el.classList.contains("cell-with-goblin")) {
-        deadCount += 1;
-        dead.textContent = deadCount;
-        if (deadCount >= 10) {
-          clearInterval(interval);
-          alert("Вы забодали гоблина!");
-          dead.textContent = 0;
-          lost.textContent = 0;
-        }
-      } else {
-        lostCount += 1;
-        lost.textContent = lostCount;
-        if (lostCount >= 10) {
-          clearInterval(interval);
-          alert("Гоблин не забодан!");
-          dead.textContent = 0;
-          lost.textContent = 0;
-        }
+        el.classList.remove('cell-with-goblin')
       }
     })
-  );
-});
+  }, 800);
+
+
+
+
+  gameplay.cells.forEach(el => el.addEventListener('click', (e) => {
+    if (el.classList.contains('cell-with-goblin')) {
+      deadCount += 1;
+      dead.textContent = deadCount;
+      if (deadCount >= 10) {
+        clearInterval(interval);
+        alert('Вы забодали гоблина!');
+        dead.textContent = 0;
+        lost.textContent = 0;
+      }
+    } else {
+      lostCount += 1;
+      lost.textContent = lostCount;
+      if (lostCount >= 10) {
+        clearInterval(interval);
+        alert('Гоблин не забодан!');
+        dead.textContent = 0;
+        lost.textContent = 0;
+      }
+    }
+  }))
+})
+
+
