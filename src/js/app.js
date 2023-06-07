8f23ece8353e1ef3e333081c5ea31bb3ef3af430
Анастасия Ельшина <salnaya92@gmail.com>
2023-06-07 13:22:11 +0300

    update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,49 +2,8 @@
 import GamePlay from "./gameplay/GamePlay";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const board = document.querySelector(".board");
-  let deadCount = 0;
-  let lostCount = 0;
-  let dead = document.querySelector(".dead-number");
-  let lost = document.querySelector(".lost-number");
   const gameplay = new GamePlay(board);
   gameplay.drawField();
-
-  const getRandom = () => Math.floor(Math.random() * gameplay.cells.length);
-  const interval = setInterval(() => {
-    let oneRandom = getRandom();
-    gameplay.cells.forEach((el, i, arr) => {
-      if (i === oneRandom) {
-        if (el === arr[i]) {
-          el.classList.add("cell-with-goblin");
-        }
-      } else {
-        el.classList.remove("cell-with-goblin");
-      }
-    });
-  }, 800);
-
-  gameplay.cells.forEach((el) =>
-    el.addEventListener("click", () => {
-      if (el.classList.contains("cell-with-goblin")) {
-        deadCount += 1;
-        dead.textContent = deadCount;
-        if (deadCount >= 10) {
-          clearInterval(interval);
-          alert("Вы забодали гоблина!");
-          dead.textContent = 0;
-          lost.textContent = 0;
-        }
-      } else {
-        lostCount += 1;
-        lost.textContent = lostCount;
-        if (lostCount >= 10) {
-          clearInterval(interval);
-          alert("Гоблин не забодан!");
-          dead.textContent = 0;
-          lost.textContent = 0;
-        }
-      }
-    })
-  );
-});
+  gameplay.onInterval()
+  gameplay.onClick();
+})
